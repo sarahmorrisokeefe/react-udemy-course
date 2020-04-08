002c2fc402ee90e09e4eb00432783d0726a537b5
Sarah Morris <sarah.morris9@gmail.com>
2020-04-08 17:49:40 -0500

    Added nav bar and side drawer for toolbar

--- burger-builder-basic/src/components/Layout/Layout.js
+++ burger-builder-basic/src/components/Layout/Layout.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Aux>
-    <div>Toolbar; SideDrawer; BackDrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
 
-export default layout;
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
