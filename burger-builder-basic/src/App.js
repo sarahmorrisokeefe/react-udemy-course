402059e8520318b12c57bfaf3b55e83b81429639
Sarah Morris <sarah.morris9@gmail.com>
2020-04-10 17:19:33 -0500

    setting up and rendering routes

--- burger-builder-basic/src/App.js
+++ burger-builder-basic/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from "./hoc/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+      <BrowserRouter>
+          <Layout>
+            <BurgerBuilder />
+          </Layout>
+      </BrowserRouter>
     );
   }
 }
