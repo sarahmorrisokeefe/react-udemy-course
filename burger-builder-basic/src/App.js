114b29bea5b17ed1a004e1c70c080ff4b33b9154
Sarah Morris <sarah.morris9@gmail.com>
2020-04-13 13:49:23 -0500

    Added checkout ability and routing

--- burger-builder-basic/src/App.js
+++ burger-builder-basic/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-          <Layout>
-            <BurgerBuilder />
-          </Layout>
-      </BrowserRouter>
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
     );
   }
 }
